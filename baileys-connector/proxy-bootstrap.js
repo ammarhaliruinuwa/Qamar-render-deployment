1a9d40b4f1b85d05492c2eb6ab3ce57b034ca1f3
ammarhaliruinuwa <halliruammar@gmail.com>
2026-09-08 12:18:47 +0100

    Add direct WhatsApp Web proxy bootstrap

---
+++ baileys-connector/proxy-bootstrap.js
@@ -0,0 +1,41 @@
+import 'global-agent/bootstrap'
+import * as https from 'node:https'
+import { HttpsProxyAgent } from 'https-proxy-agent'
+
+const proxyUrl = process.env.BAILEYS_PROXY_URL || process.env.GLOBAL_AGENT_HTTP_PROXY || ''
+const proxyAgent = proxyUrl ? new HttpsProxyAgent(proxyUrl) : null
+
+if (proxyAgent) {
+  const originalRequest = https.request
+
+  https.request = function patchedHttpsRequest(...args) {
+    let options = args[0]
+    let hostname = ''
+
+    if (typeof options === 'string' || options instanceof URL) {
+      const parsed = new URL(options)
+      hostname = parsed.hostname
+      options = parsed
+    } else if (options && typeof options === 'object') {
+      hostname = String(options.hostname || options.host || '').split(':')[0]
+    }
+
+    if (hostname === 'web.whatsapp.com' || hostname.endsWith('.web.whatsapp.com')) {
+      if (options instanceof URL) {
+        options = new URL(options.toString())
+        options.agent = proxyAgent
+      } else {
+        options = { ...options, agent: proxyAgent }
+      }
+      args[0] = options
+    }
+
+    return originalRequest.apply(this, args)
+  }
+
+  console.log('Qamar WhatsApp HTTPS proxy agent enabled')
+} else {
+  console.log('Qamar WhatsApp proxy agent not configured')
+}
+
+await import('./server.js')
